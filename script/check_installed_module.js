4e48219500266e8f6865764bf80d6bac4446d28f
ppxbp <93709219+ppxbpfiles@users.noreply.github.com>
2026-02-20 11:35:21 +0900

    20260218

--- script/check_installed_module.js
+++ script/check_installed_module.js
@@ -28,20 +28,22 @@ var basePath = PPx.Extract("%0");
 
 // チェック対象モジュールリスト
 var modules = [
-  //PPx拡張モジュール
-  { file: "PPxccx64.dll", name: "PPx CCX Module", path: "" },
-  { file: "PPxcfx64.dll", name: "PPx CFX Module", path: "" },
-  { file: "PPxetp64.dll", name: "PPx Everything Search Module", path: "" },
-  { file: "PPxjunk64.dll", name: "PPx Junk Module", path: "" },
-  { file: "PPxmes64.dll", name: "PPx Message Module", path: "" },
-  { file: "PPxscr64.dll", name: "PPx Script Module", path: "" },
-  { file: "PPxtext64.dll", name: "PPx Text Module", path: "" },
-  { file: "PPxwin64.dll", name: "PPx Window Module", path: "" },
-  { file: "PPxws64.dll", name: "PPx WS Module", path: "" },
-  //正規表現・migemoモジュール
-  { file: "bregonig.dll", name: "bregonig.dll", path: "" },
-  { file: "migemo.dll", name: "migemo.dll", path: "" },
-  // 圧縮・解凍・統合アーカイバDLL
+  // PPx拡張モジュール (P_module)
+  { file: "ppxccx64.dll", name: "PPx CCX Module", path: "" },
+  { file: "PPXCFX64.DLL", name: "PPx CFX Module", path: "" },
+  { file: "PPXETP64.DLL", name: "PPx Everything Search Module", path: "" },
+  { file: "PPXJUNK64.DLL", name: "PPx Junk Module", path: "" },
+  { file: "PPXMES64.DLL", name: "PPx Message Module", path: "" },
+  { file: "PPXSCR64.DLL", name: "PPx Script Module", path: "" },
+  { file: "PPXTEXT64.DLL", name: "PPx Text Module", path: "" },
+  { file: "PPXWIN64.DLL", name: "PPx Window Module", path: "" },
+  { file: "ppxws64.DLL", name: "PPx WS Module", path: "" },
+
+  // 正規表現・migemoモジュール (CFGに直接記載はないが必須)
+  { file: "bregonig.dll", name: "BREGEXP.DLL", path: "" },
+  { file: "migemo.dll", name: "CMigemo.DLL", path: "" },
+
+  // 統合アーカイバDLL (P_arc) と圧縮EXEファイル
   { file: "7z.dll", name: "7-zip DLL", path: "" },
   { file: "7z.exe", name: "7-zip EXE", path: "" },
   { file: "rar.exe", name: "console RAR EXE", path: "tools\\" },
@@ -54,38 +56,64 @@ var modules = [
   { file: "unlha32.dll", name: "UNLHA32 DLL", path: "" },
   { file: "cab32.dll", name: "CAB32 DLL", path: "" },
   { file: "tar64.dll", name: "TAR64 DLL", path: "" },
-  //Susieプラグイン
-  { file: "iftwic.sph", name: "iftwic.sph", path: "" },
-  { file: "ifjpegt.sph", name: "ifjpegt.sph", path: "" },
-  { file: "iftpsd.sph", name: "iftpsd.sph", path: "" },
-  { file: "ifttf.sph", name: "ifttf.sph", path: "" },
-  { file: "ifextend.sph", name: "ifextend.sph", path: "" },
-  { file: "ifheif.sph", name: "ifheif.sph", path: "" },
+
+  // Susieプラグイン (P_susie)
+  { file: "axpdf.sph", name: "axpdf.sph", path: "" },
   { file: "axtpsd.sph", name: "axtpsd.sph", path: "" },
   { file: "axttc.sph", name: "axttc.sph", path: "" },
+  { file: "axvarfont.sph", name: "axvarfont.sph", path: "" },
+  { file: "axwoff.sph", name: "axwoff.sph", path: "" },
+  { file: "axwoff2.sph", name: "axwoff2.sph", path: "" },
+  { file: "ifextend.sph", name: "ifextend.sph", path: "" },
   { file: "ifgif.sph", name: "ifgif.sph", path: "" },
+  { file: "ifheif.sph", name: "ifheif.sph", path: "" },
+  { file: "ifjpegt.sph", name: "ifjpegt.sph", path: "" },
   { file: "iftgdip.sph", name: "iftgdip.sph", path: "" },
+  { file: "iftpsd.sph", name: "iftpsd.sph", path: "" },
+  { file: "ifttf.sph", name: "ifttf.sph", path: "" },
   { file: "iftwebp.sph", name: "iftwebp.sph", path: "" },
-  { file: "axpdf.sph", name: "axpdf.sph", path: "" },
+  { file: "iftwic.sph", name: "iftwic.sph", path: "" },
   { file: "pdfium.dll", name: "axpdf.sph用DLL", path: "" },
+
   //Susieプラグイン（SPI）
   { file: "ifcrw_t.spi", name: "ifcrw_t.spi", path: "SPI\\" },
   { file: "ifdxfft.spi", name: "ifdxfft.spi", path: "SPI\\" },
   { file: "ifdwg.spi", name: "ifdwg.spi", path: "SPI\\" },
   { file: "runspi.exe", name: "runspi.exe", path: "SPI\\" },
+
   //その他外部ツール
+  { file: "azure_cui.exe", name: "azurecui", path: "tools\\" },
+  { file: "bulkfilechanger.exe", name: "bulkfilechanger", path: "tools\\" },
   { file: "busybox64u.exe", name: "busybox64", path: "" },
+  { file: "CT.exe", name: "CT", path: "tools\\" },
+  { file: "DialogHandler.x86-64.exe", name: "dialoghandler", path: "tools\\" },
+  { file: "es.exe", name: "everything cli", path: "tools\\" },
+  { file: "exiftool.exe", name: "exiftool", path: "tools\\" },
+  { file: "fastcopy.exe", name: "fastcopy", path: "tools\\" },
+  { file: "ffmpeg.exe", name: "ffmpeg", path: "tools\\" },
+  { file: "filesum.exe", name: "filesum", path: "tools\\" },
+  { file: "filetypesman.exe", name: "filetypesman", path: "tools\\" },
+  { file: "GB.exe", name: "GB", path: "tools\\" },
+  { file: "ish.exe", name: "ish", path: "tools\\" },
+  { file: "ls2lf.exe", name: "ls2lf", path: "auxcmd\\" },
+  { file: "magick.exe", name: "magick", path: "tools\\" },
+  { file: "mediainfo.exe", name: "mediainfoCLI", path: "tools\\" },
+  { file: "nkf.exe", name: "nkf", path: "tools\\" },
+  { file: "oscdimg.exe", name: "oscdimg", path: "tools\\" },
+  { file: "pandoc.exe", name: "pandoc", path: "tools\\" },
+  { file: "pathtoclip.exe", name: "pathtoclip", path: "tools\\" },
+  { file: "pdfcpu.exe", name: "pdfcpu", path: "tools\\" },
+  { file: "rapidee.exe", name: "rapidee", path: "tools\\" },
+  { file: "rg.exe", name: "ripgrep", path: "tools\\" },
+  { file: "shexview.exe", name: "shellexview", path: "tools\\" },
+  { file: "shmnview.exe", name: "shellmenuview", path: "tools\\" },
+  { file: "simplemount.exe", name: "simplemount", path: "tools\\" },
+  { file: "xdoc2txt.exe", name: "xdoc2txt", path: "tools\\" },
+  { file: "zoxide.exe", name: "zoxide", path: "tools\\" },
   { file: "ppdex64.exe", name: "PPx common dialog extender", path: "" },
   { file: "ppdex.exe", name: "PPx common dialog extender", path: "" },
   { file: "ppdex64.dll", name: "PPx common dialog extender DLL", path: "" },
   { file: "ppdex.dll", name: "PPx common dialog extender DLL", path: "" },
-  { file: "xdoc2txt.exe", name: "xdoc2txt", path: "tools\\" },
-  { file: "zoxide.exe", name: "zoxide", path: "tools\\" },
-  { file: "es.exe", name: "everything cli", path: "tools\\" },
-  { file: "exiftool.exe", name: "exiftool", path: "tools\\" },
-  { file: "rg.exe", name: "ripgrep", path: "tools\\" },
-  { file: "fcp.exe", name: "fastcopy cli", path: "tools\\" },
-  { file: "ls2lf.exe", name: "ls2lf", path: "auxcmd\\" },
 ];
 
 // 欠如モジュールのエラーメッセージ収集
@@ -96,7 +124,7 @@ for (var i = 0; i < modules.length; i++) {
   // ファイルが存在しない場合のみエラーに追加
   if (!fs.FileExists(fullPath)) {
     errorMessages.push(
-      m.name + "（%0" + m.path + m.file + "）がインストールされていません。"
+      m.name + "（%0" + m.path + m.file + "）がインストールされていません。",
     );
   }
 }
@@ -115,7 +143,9 @@ if (!forceMode) {
 
 // ダイアログ表示（「はい」=以降非表示、「いいえ」=今後も表示）
 var result = PPx.Extract(
-  '%*choice(-title:"check_installed_module.js" -type:yn -text"' + message + '")'
+  '%*choice(-title:"check_installed_module.js" -type:yn -text"' +
+    message +
+    '")',
 );
 if (!forceMode && result === "1") {
   PPx.Execute("*setcust _user:checkmodule=0");
