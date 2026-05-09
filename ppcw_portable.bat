aa9d33ba84aa1779d21d173d836cac46400bbf7a
ppxbp <93709219+ppxbpfiles@users.noreply.github.com>
2026-05-09 15:44:28 +0900

    20260509

--- ppcw_portable.bat
+++ ppcw_portable.bat
@@ -1,11 +1,13 @@
 @echo off
 rem fonton(ver1.1以降)を使ってフォントをシステムにインストールせず一時登録してPPxを使う。
+rem このファイルはPPx本体をインストールしているフォルダに配置する。
+rem fontonは https://toro.d.dooo.jp/slwin4.html#fonton または、https://github.com/toroidj/toroidj.github.io/blob/master/win/fonton11.zip からDLする。
 
 rem PPxをインストールしているディレクトリに移動
 pushd "%~dp0"
 
 rem fontonを起動しフォントを一時登録
-rem PPx終了（最後の1窓を閉じる）ときにfontrを実行するよう、CLOSEEVENTにコマンドを書き込む。1度実行したらそのコマンドは消去。
+rem PPx終了（最後の1窓を閉じる）ときにfonton -s -rを実行してフォント登録解除するよう、CLOSEEVENTにコマンドを書き込む。1度実行したらそのコマンドは消去。
 call fonton64.exe
 
 timeout /t 1
